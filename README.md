8d9fe4e7ea3ad0ecae8bf2f663e18ee339e3ce99
hasantayyar <tayyar.besik@gmail.com>
2014-07-27 04:53:41 +0300

    updated data source

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 This project uses Yahoo pipes to get flickr images as datasource.
 
+```bash
+curl -XGET http://dummy-food.herokuapp.com/
+```
--- index.php
+++ index.php
@@ -1,6 +1,30 @@
 <?php
 include './vendor/autoload.php';
 use Hasantayyar\GetPost;
-$get = new Hasantayyar\GetPost\Get('http://pipes.yahoo.com/pipes/pipe.run?FlickrID=69884632%40N00&Size=Large&Tagmode=all&Tags=travel&_id=59ee17da00a2c6b5af4eede29185a9b4&_render=json');
-$data =  json_decode($get->send());
-print_r($data); 
+
+$cacheFile = './cache/'.date('HdmY',time());
+if(file_exists($cacheFile)){
+	$jsonData = file_get_contents($cacheFile);	
+}else{
+	$url = 'http://pipes.yahoo.com/pipes/pipe.run'; //'FlickrID=69884632%40N00&Size=Large&Tagmode=all&Tags=travel&_id=59ee17da00a2c6b5af4eede29185a9b4&_render=json';
+	$tags = 'food';
+	$params = array(
+	//'FlickrID'=>'7204959@N04',
+	'Size'=>'Medium', //Square',
+	'Tagmode'=>'all',
+	'Tags'=>$tags,
+	'_id'=>'59ee17da00a2c6b5af4eede29185a9b4',
+	'_render'=>'json'
+	);
+	$fullUrl = $url.'?'.http_build_query($params);
+	$get = new Hasantayyar\GetPost\Get($fullUrl);
+	$jsonData = $get->send();
+	$f = fopen($cacheFile,'w');
+	fwrite($f,$jsonData);
+	fclose($f);
+}
+
+$data =  json_decode($jsonData);
+$rand = array_rand($data->value->items); 
+
+echo json_encode($data->value->items[$rand]);
